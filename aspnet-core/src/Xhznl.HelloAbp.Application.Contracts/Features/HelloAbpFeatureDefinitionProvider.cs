1f828378f2974b303a9f490150f32acd71ba18c1
xiajr <xiajr@outlook.com>
2020-09-03 16:18:24 +0800

    add organizationuinit api,add features

---
+++ aspnet-core/src/Xhznl.HelloAbp.Application.Contracts/Features/HelloAbpFeatureDefinitionProvider.cs
@@ -0,0 +1,26 @@
+﻿using Volo.Abp.Features;
+using Volo.Abp.Localization;
+using Volo.Abp.Validation.StringValues;
+using Xhznl.HelloAbp.Localization;
+using Xhznl.HelloAbp.Permissions;
+
+namespace Xhznl.HelloAbp.Features
+{
+    public class HelloAbpFeatureDefinitionProvider : FeatureDefinitionProvider
+    {
+        public override void Define(IFeatureDefinitionContext context)
+        {
+            var group = context.AddGroup(HelloAbpFeatures.GroupName);
+
+            group.AddFeature(HelloAbpFeatures.SocialLogins, "true", L("Feature:SocialLogins")
+                , valueType: new ToggleStringValueType());
+            group.AddFeature(HelloAbpFeatures.UserCount, "10", L("Feature:UserCount")
+                , valueType: new FreeTextStringValueType(new NumericValueValidator(1, 1000)));
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<HelloAbpResource>(name);
+        }
+    }
+}
