f745066c3f3c66f6a043fe60610c2808a1ab2493
xiajr <xiajr@outlook.com>
2020-09-05 21:55:07 +0800

    fixed a problem with OU localization resoures

--- aspnet-core/src/Xhznl.HelloAbp.Application.Contracts/Permissions/HelloAbpPermissionDefinitionProvider.cs
+++ aspnet-core/src/Xhznl.HelloAbp.Application.Contracts/Permissions/HelloAbpPermissionDefinitionProvider.cs
@@ -2,6 +2,7 @@
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Localization;
 using Volo.Abp.Identity;
+using Volo.Abp.Identity.Localization;
 
 namespace Xhznl.HelloAbp.Permissions
 {
@@ -11,21 +12,24 @@ namespace Xhznl.HelloAbp.Permissions
         {
             var identityGroup = context.GetGroup(IdentityPermissions.GroupName);
 
-            var ouPermission = identityGroup.AddPermission(HelloIdentityPermissions.OrganitaionUnits.Default, L("Permission:OrganitaionUnitManagement"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Create, L("Permission:Create"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Update, L("Permission:Edit"));
-            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Delete, L("Permission:Delete"));
+            var ouPermission = identityGroup.AddPermission(HelloIdentityPermissions.OrganitaionUnits.Default, IdentityL("Permission:OrganitaionUnitManagement"));
+            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Create, IdentityL("Permission:Create"));
+            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Update, IdentityL("Permission:Edit"));
+            ouPermission.AddChild(HelloIdentityPermissions.OrganitaionUnits.Delete, IdentityL("Permission:Delete"));
 
             var userPermission = identityGroup.GetPermissionOrNull(IdentityPermissions.Users.Default);
-            if (userPermission!=null)
-            {
-                userPermission.AddChild(HelloIdentityPermissions.Users.DistributionOrganizationUnit, L("Permission:DistributionOrganizationUnit"));
-            }
+            userPermission?.AddChild(HelloIdentityPermissions.Users.DistributionOrganizationUnit, IdentityL("Permission:DistributionOrganizationUnit"));
         }
 
         private static LocalizableString L(string name)
         {
             return LocalizableString.Create<HelloAbpResource>(name);
         }
+
+        private static LocalizableString IdentityL(string name)
+        {
+            return LocalizableString.Create<IdentityResource>(name);
+        }
+
     }
 }
